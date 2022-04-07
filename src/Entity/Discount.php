da9b4aeda1d6886e8ad89f489bb1438e7f203887
Francois Geffriaud <francoisgeffriaud@gmail.com>
2022-04-07 16:40:43 +0200

    crud discount
    ajout/modififs/supp/liste des discount dans le back office

---
+++ src/Entity/Discount.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DiscountRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DiscountRepository::class)]
+class Discount
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $type;
+
+    #[ORM\Column(type: 'integer')]
+    private $validity;
+
+    #[ORM\Column(type: 'text')]
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getValidity(): ?int
+    {
+        return $this->validity;
+    }
+
+    public function setValidity(int $validity): self
+    {
+        $this->validity = $validity;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
