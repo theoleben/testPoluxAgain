717bf0181b76cee29e097db4845e56cffa81d8d6
Théo LEBEN <theo.leben@gmail.com>
2022-04-07 16:15:39 +0200

    feature: Abonnements
    - Back office : Ajout/Modification/Suppression des formules d'abonnement
    - Client : Affichage des abonnements via le HomeController

---
+++ src/Entity/Subscription.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubscriptionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SubscriptionRepository::class)]
+class Subscription
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'integer')]
+    private $games_number;
+
+    #[ORM\Column(type: 'integer')]
+    private $price;
+
+    #[ORM\Column(type: 'integer')]
+    private $new_duration;
+
+    #[ORM\Column(type: 'integer')]
+    private $shipping_cost;
+
+    #[ORM\Column(type: 'text')]
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getGamesNumber(): ?int
+    {
+        return $this->games_number;
+    }
+
+    public function setGamesNumber(int $games_number): self
+    {
+        $this->games_number = $games_number;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getNewDuration(): ?int
+    {
+        return $this->new_duration;
+    }
+
+    public function setNewDuration(int $new_duration): self
+    {
+        $this->new_duration = $new_duration;
+
+        return $this;
+    }
+
+    public function getShippingCost(): ?int
+    {
+        return $this->shipping_cost;
+    }
+
+    public function setShippingCost(int $shipping_cost): self
+    {
+        $this->shipping_cost = $shipping_cost;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
---
+++ src/Form/SubscriptionType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Subscription;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SubscriptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('games_number')
+            ->add('price')
+            ->add('new_duration')
+            ->add('shipping_cost')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Subscription::class,
+        ]);
+    }
+}
