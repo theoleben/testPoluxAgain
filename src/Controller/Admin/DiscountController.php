3189b3f438416fc2338a0c810cbc3d8fdb56bfec
Théo LEBEN <theo.leben@gmail.com>
2022-04-07 17:48:29 +0200

    branch: develop
    Modification liée au merge

--- src/Controller/Admin/DiscountController.php
+++ src/Controller/Admin/DiscountController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-// #[Route('/admin/discount')]
+#[Route('/admin/discount')]
 class DiscountController extends AbstractController
 {
     #[Route('/', name: 'app_admin_discount_index', methods: ['GET'])]
