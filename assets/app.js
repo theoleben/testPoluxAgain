85c83fae03a6c4f6506d0f89bbf02bbb56cdf1f7
SophieMispelter <asmispelter@yahoo.fr>
2022-04-15 11:48:29 +0200

    commit 15/04/2022
    travail front en cours

--- assets/app.js
+++ assets/app.js
@@ -15,3 +15,4 @@ const $ = require('jquery'); // pour utiliser jquery
 global.$ = global.jQuery = $; // fix un pb d'utilisation de la variable $
 
 require('bootstrap'); // pour utiliser le js de bootstrap
+
